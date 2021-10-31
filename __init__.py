befda25a16fb95b7a3f8f5027c77ac6547a05cf3
Fumiya Shibamata <mail@sbfm.jp>
2021-10-31 12:33:39 +0900

    first Commit.

---
+++ __init__.py
@@ -0,0 +1,52 @@
+# coding: utf-8
+#
+#  外部ファイル読み込み処理のキャッシュ化
+#  
+# Licence : MIT Licence
+# owner   : Fumiya Shibamata
+#
+
+import os
+import pickle
+
+# 管理名、関数、管理ディレクトリパス
+def get(modelName, loadFunction, type=1, dir="./model/"):
+    """
+    値の取得
+        modelName : 保存するバイナリデータのファイル名
+        loadFunction : キャッシュが無効の時に実行される取得処理
+        type : 将来的に実装される強制読み込み条件
+        dir : モデルデータを管理するディレクトリ(未指定時 ./ )
+    """
+    
+    # ----------------------
+    # キャッシュを利用していいか条件を満たしているか確認
+    # ----------------------
+    if os.path.isfile(dir + modelName + ".model"):
+        # キャッシュの読み込み
+        return_value = _load_dictionary(dir + modelName + ".model")
+    else :
+        # 値の取得
+        return_value = loadFunction()
+        # キャッシュの保存
+        _save_dictionary(dir + modelName + ".model", return_value)
+    return return_value
+
+def _save_dictionary(file, values):
+    """
+    バイナリデータの保存
+        file : 保存するバイナリデータのファイル名
+    """
+    f = open(file, 'wb')
+    pickle.dump(values, f)
+    f.close()
+
+def _load_dictionary(file):
+    """
+    バイナリデータの読み込み
+        file : 保存するバイナリデータのファイル名
+    """
+    f = open(file, 'rb')
+    loadFile = pickle.load(f)
+    f.close()
+    return loadFile    
